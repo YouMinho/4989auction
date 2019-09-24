2e1d5edf21980e17d89ab92920dc802b0348d768
minho3103@gmail.com <minho3103@gmail.com>
2019-09-24 15:10:39 +0900

    head,logo,footer -> ejs

---
+++ app.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const app = express();
+
+app.use(express.static('public'));
+app.set('views', './views');
+app.set('view engine', 'ejs');
+
+
+app.get('/', (req, res) => {
+    res.render('main')
+})
+
+app.listen(8888, () => {
+    console.log('8888 port opened!!!');
+})
