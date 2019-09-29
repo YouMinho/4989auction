c49255949a108b1ff839eab8aef186b92b3f372e
kangG13 <dmssk13@gmail.com>
2019-09-29 14:45:27 +0900

    회원가입 기능 구현

--- app.js
+++ app.js
@@ -54,14 +54,12 @@ app.get('/', (req, res) => {
             console.log(err);
             res.status(500).send('Internal Server Error!!!')
         }
-        console.log(res.locals.user);
         res.render('main', { article: results, category: 'ALL' });
     })
 })
 
 app.get('/login', (req, res) => {
     const sess = req.session;
-
     res.render('login', { pass: true });
 })
 
@@ -128,9 +126,35 @@ app.get('/item_info/:num', (req, res) => {
         res.render('item_info', { article: results[0] })
     })
 })
+
 app.get('/signup', (req, res) => {
-    res.render('signup')
-})
+    res.render('signup');
+});
+
+app.post('/signup', (req, res) => {
+    const sess = req.session;
+
+    let id = req.body.id;
+    let name = req.body.name;
+    let password = req.body.pass;
+    let email = req.body.emailid + "@" + req.body.emaildomain;
+    let phone = req.body.tel1 + req.body.tel2 + req.body.tel3;
+    let address = req.body.address;
+
+	let values = [id, password, "G", name, email, phone, address];
+	let users_insert = `
+	insert into users (id, password, grade, name, email, phone, address)
+	values(?, ?, ?, ?, ?, ?, ?)
+	`;
+	connection.query(users_insert, values, (err, result) => {
+        sess.userid = id;
+        sess.name = name;
+        sess.grade = "G";
+        req.session.save(() => {
+            res.redirect('/');
+        });
+	});
+});
 
 app.get('/find_idpw', (req, res) => {
     res.render('find_idpw')
